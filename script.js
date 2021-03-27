321bf9ad1d7173462815a0b2c2063f4aee2a5f1a
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-03-27 22:02:01 +0200

    create all elements for minimal version

--- script.js
+++ script.js
@@ -1,12 +1,68 @@
 //You can edit ALL of the code here
+
+//Zero-pad numbers to two digits.
+function padLeadingZeros(num, size) {
+  let smallNumber = "" + num;
+  while (smallNumber.length < size) {
+    smallNumber = "0" + smallNumber;
+    return smallNumber;
+  }
+}
+
+// Website where data originally comes from. 
+const dataSource = "TVMaze.com";
+
+//  Add link back to original site.
+let footerElement = document.createElement("footer");
+document.body.appendChild(footerElement);
+footerElement.innerHTML = `The data originally comes from ${dataSource.link("https://www.tvmaze.com/shows/82/game-of-thrones")}`;
+
+//  Given source code.
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
+//  Given source code.
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  
+  
+  //  Looping through the episode list.
+  episodeList.forEach(episodeInList => {
+    // console.log("Episode Name:", episodeInList.name);
+    // console.log("Episode Season",episodeInList.season);
+    // console.log("Episode Number",episodeInList.number);
+    // console.log("Episode Medium Image",episodeInList.image.medium);
+    
+    //  Create div for a card.
+    let cardDiv = document.createElement("div");
+    rootElem.appendChild(cardDiv);
+    cardDiv.className = "card";
+    // console.log("This is my cardDiv", cardDiv);
+    
+    //  Create a div with class of container for styling later.
+    let containerDiv = document.createElement("div");
+    cardDiv.appendChild(containerDiv);
+    containerDiv.className = "container";
 
+    //  Create the heading for the card.
+    let heading = document.createElement("h4")
+    containerDiv.appendChild(heading);
+    containerDiv.textContent = `${episodeInList.name} - S${padLeadingZeros(episodeInList.season, 2)}E${padLeadingZeros(episodeInList.number, 2)}`;
+    
+    //  Create the image for the card.
+    let imgElement = document.createElement("img");
+    containerDiv.appendChild(imgElement);
+    imgElement.src = episodeInList.image.medium;
+    imgElement.style.width = "100%";
+    
+    //  Create the paragraph for the card.
+    let paragraphElement = document.createElement("p");
+    containerDiv.appendChild(paragraphElement);
+    paragraphElement.innerHTML = episodeInList.summary;    
+  })
+  
+};
 window.onload = setup;
